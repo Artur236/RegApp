81a560e5ea37c032c87a2c8eba5c157e7d4d0540
Arthur Poliakov <poliakov.arthur@gmail.com>
2017-01-07 12:56:57 +0200

    add master page

---
+++ src/pages/MasterPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Link} from 'react-router';
+import {LoginLink} from 'react-stormpath';
+import DocumentTitle from 'react-document-title';
+
+import Header from './Header';
+
+export default class is extends React.Component {
+    render() {
+        return (
+            <DocumentTitle title='Registration app'>
+                <div className='MasterPage'>
+                    <Header/> {this.props.children}
+                </div>
+            </DocumentTitle>
+        );
+    }
+}
